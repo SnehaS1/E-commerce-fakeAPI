808a63248bf24d69a11fd2f82a4d4b63706c245d
Sneha S <snehasushatha@gmail.com>
2022-12-16 08:54:13 +0400

    navbar pages

--- e-commerce-app/src/App.tsx
+++ e-commerce-app/src/App.tsx
@@ -1,7 +1,24 @@
+import { Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap'
+import Home from './pages/Home';
+import Store from './pages/Store';
+import About from './pages/About';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <>
+      <Navbar></Navbar>
+      <Container className='mb-4'>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/store' element={<Store />}></Route>
+          <Route path='/about' element={<About />}></Route>
+
+        </Routes>
+      </Container>
+    </>
+
   );
 }
 
